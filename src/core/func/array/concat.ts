00154626bad960d4f9c4eeb68d683f07e2db809a
isaikin.victor <isaikin@bezlimit.ru>
2022-09-20 22:57:12 +0600

    with concat, concatUnique

--- src/core/func/array/concat.ts
+++ src/core/func/array/concat.ts
@@ -1,6 +1,6 @@
 import { Many } from '../../model/common';
 
 export default function concat<T>(...args: Many<T>[]): T[] {
-  const res: T[] = ([] as T[]).concat.apply([], args);
+  const res: T[] = ([] as T[]).concat(...args);
   return res;
 }
